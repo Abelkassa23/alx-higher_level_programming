ae14fe6cda23935b6b2eb3d7233a122b9a099b08
Abelkassa23 <99481697+Abelkassa23@users.noreply.github.com>
2022-06-08 20:11:50 +0300

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,32 +1,13 @@
 #!/usr/bin/python3
-# 12-roman_to_int.py
-
-
 def roman_to_int(roman_string):
-    """Convert roman numeral to int."""
-    if roman_string != str(roman_string) or
-            roman_string is None):
-        return (0)
-
-    roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
-    num = 0
-
-    for j in range(len(roman_string)):
-        if roman_dict.get(roman_string[j], 0) == 0:
-            return (0)
-
-        if (j != (len(roman_string) - 1) and
-                roman_dict[roman_string[j]] < roman_dict[roman_string[j + 1]]):
-                num += roman_dict[roman_string[j]] * -1
-
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+    rel = 0
+    rd = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    chars = len(roman_string)
+    for i in range(chars):
+        if i != chars - 1 and (rd[roman_string[i + 1]] > rd[roman_string[i]]):
+            rel -= rd[roman_string[i]]
         else:
-            num += roman_dict[roman_string[j]]
-    return (num)
+            rel += rd[roman_string[i]]
+    return rel
