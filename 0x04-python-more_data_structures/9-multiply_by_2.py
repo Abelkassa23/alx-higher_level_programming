0e8dee2014dd3ce2b03ab0bdf206a2405d560ed0
Abelkassa23 <99481697+Abelkassa23@users.noreply.github.com>
2022-06-08 20:04:38 +0300

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+# 9-multiple_by_2.py
+
+
 def multiply_by_2(a_dictionary):
-        container = a_dictionary.copy()
-        for run in a_dictionary:
-                container[run] = container[run] * 2
-        return container
+    """Return a new dict with vals multipled by 2."""
+    return ({i: a_dictionary[i] * 2 for i in a_dictionary})
